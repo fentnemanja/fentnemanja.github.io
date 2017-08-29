e86375b398a9756578661c993d3db81d8f0fc35b
Nemanja Fent <fent.96n@gmail.com>
2017-08-29 22:12:36 +0200

    responsive

--- js/app.js
+++ js/app.js
@@ -4,4 +4,10 @@ $(document).ready(function () {
   jQuery(document).ready(function(){
     jQuery('#slippry-demo').slippry()
   });
+
+  $('.burger').click(function() {
+      $('nav').stop().slideToggle('medium');
+      $('.flags').stop().slideToggle('medium');
+      return $(this).toggleClass('open');
+  });
 });
