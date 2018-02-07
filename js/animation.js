d73c6144fdc57bf25e834c9b7bf4ac2edb90e844
Nemanja Fent <fent.96n@gmail.com>
2018-02-07 21:43:01 +0100

    Update animation.js

--- js/animation.js
+++ js/animation.js
@@ -7,26 +7,6 @@ const design = document.querySelector('.js-design');
 
 const header = document.querySelector('.js-header');
 
-function getBrowserSize(){
-    var w, h;
-
-      if(typeof window.innerWidth != 'undefined')
-      {
-       w = window.innerWidth; //other browsers
-       h = window.innerHeight;
-      } 
-      else if(typeof document.documentElement != 'undefined' && typeof      document.documentElement.clientWidth != 'undefined' && document.documentElement.clientWidth != 0) 
-      {
-       w =  document.documentElement.clientWidth; //IE
-       h = document.documentElement.clientHeight;
-      }
-      else{
-       w = document.body.clientWidth; //IE
-       h = document.body.clientHeight;
-      }
-    return {'width':w, 'height': h};
-}
-
 TweenMax.from(text, 1.5, {
     delay: .5,
     y: 100,
@@ -38,46 +18,6 @@ TweenMax.from(header, 1.5, {
     opacity: 0
 });
 
-
-if(parseInt(getBrowserSize().width) > 750){
-    
-    
-    TweenMax.from(html, 1.5, {
-        delay: 1.5,
-        ease: Power3.easeOut,
-        opacity: .4,
-        width: 0
-    });
-    
-    TweenMax.from(css, 1.5, {
-        ease: Power3.easeOut,
-        delay: 1.8,
-        opacity: .4,
-        width: 0
-    });
-    
-    TweenMax.from(js, 1.5, {
-        ease: Power3.easeOut,
-        delay: 2,
-        opacity: .4,
-        width: 0
-    });
-    
-    TweenMax.from(bootstrap, 1.5, {
-        ease: Power3.easeOut,
-        delay: 2.2,
-        opacity: .4,
-        width: 0
-    });
-    
-    TweenMax.from(design, 1.5, {
-        ease: Power3.easeOut,
-        delay: 2.6,
-        opacity: .4,
-        width: 0
-    });
-}
-    
 
 
 
