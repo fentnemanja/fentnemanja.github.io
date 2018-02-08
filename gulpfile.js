c37428192e32f0cbb3860bd0643c0b763af4305e
Nemanja Fent <fent.96n@gmail.com>
2018-02-08 20:57:58 +0100

    packgage.json and gulpfile added

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp			= require('gulp'),
+	gutil           = require('gulp-util'),
+	sass			= require('gulp-sass'),
+	autoprefixer	= require('autoprefixer')
+	postcss          = require('gulp-postcss'),
+	flexBugsFix      = require('postcss-flexbugs-fixes'),
+	
+
+gulp.task('scss', function() {
+	var processors = [
+		autoprefixer({ browsers: ['last 3 versions', 'ios >= 6'] }),
+		flexBugsFix
+	];
+
+	return gulp.src('scss/**/*.scss')
+		.pipe(sass().on('error', sass.logError))
+		.pipe(postcss(processors))
+		.pipe(gulp.dest('./css'));
+});
+
+
+gulp.task('watch', function() {
+	gulp.watch('scss/**/*.scss', ['scss']);
+})
